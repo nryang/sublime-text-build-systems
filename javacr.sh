5bfb5207ba66ba9e9879b895713bcab33b661984
Nathan Yang <nryang@server.fake>
2013-03-15 10:38:26 -0400

    adjusted style

--- javacr.sh
+++ javacr.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-javac $1.java
-if [ $? == 0 ]; then {
-        java $1
-	} else {
-	echo "Compiling error, no .class file created"
-	} fi
 
+javac $1.java
+
+if [ $? == 0 ]; then {
+	java $1
+
+} else {
+	echo "Compiling error, no .class file created"
+}
+
+fi
--- ocamlcr.sh
+++ ocamlcr.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 ocamlc $1.ml -o $1
+
 if [ $? == 0 ]; then {
 	rm $1.cmo $1.cmi
-        ./$1
-	} else {
-	echo "Compiling error, no .cmo and .cmi files created"
-	} fi
+	./$1
 
+} else {
+	echo "Compiling error, no .cmo and .cmi files created"
+}
+
+fi
